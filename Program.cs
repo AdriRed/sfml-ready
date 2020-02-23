1539c1ef7ef9e9c79a196b74c6ca5b4aae7dbfc8
AdriRed <adriamilmarcos@gmail.com>
2020-02-23 19:31:27 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sfml_ready
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
